07fa0124ef3214a4d0d78ca94c20d78b76ee3bec
lizichen <lizichen.qd@gmail.com>
2018-12-14 22:43:56 +0800

    finish day 7

--- day7/part1/part1.go
+++ day7/part1/part1.go
@@ -8,33 +8,43 @@ import (
 	"sort"
 )
 
+type Node struct {
+	Prev, Next []string
+}
+
 func main() {
-	f, _ := os.Open("../sample")
+	f, _ := os.Open("../input")
 	defer f.Close()
 	file := bufio.NewScanner(f)
-	steps := make(map[string]string)
-	nodes := make(map[string][]string)
+	nodes := make(map[string]*Node)
 	for file.Scan() {
 		input := file.Text()
 		regex := regexp.MustCompile(`Step ([A-Z]) must be finished before step ([A-Z]) can begin.`)
 		res := regex.FindStringSubmatch(input)
-
-		nodes[res[1]] = append(nodes[res[1]], res[2])
-		steps[res[2]] = res[1]
-	}
-	roots := make(map[string]bool)
-	for _, value := range steps {
-		_, ok := steps[value]
+		_, ok := nodes[res[1]]
 		if !ok {
-			roots[value] = true
+			nodes[res[1]] = &Node{}
 		}
+		_, ok = nodes[res[2]]
+		if !ok {
+			nodes[res[2]] = &Node{}
+		}
+		nodes[res[1]].Next = append(nodes[res[1]].Next, res[2])
+		nodes[res[2]].Prev = append(nodes[res[2]].Prev, res[1])
 	}
 	var next []string
-	for key, _ := range roots {
-		next = append(next, key)
+	for key, value := range nodes {
+		if value.Prev == nil {
+			next = append(next, key)
+		}
 	}
-	fmt.Println(nodes)
+	for key, value := range nodes {
+		fmt.Println(key)
+		fmt.Println(*value)
+	}
+
 	seen := make(map[string]bool)
+loop:
 	for len(next) > 0 {
 		sort.StringSlice(next).Sort()
 		_, ok := seen[next[0]]
@@ -42,9 +52,16 @@ func main() {
 			next = next[1:]
 			continue
 		}
+		for _, s := range nodes[next[0]].Prev {
+			_, ok = seen[s]
+			if !ok {
+				next = next[1:]
+				continue loop
+			}
+		}
 		seen[next[0]] = true
 		fmt.Print(next[0])
-		next = append(next, nodes[next[0]]...)
+		next = append(next, nodes[next[0]].Next...)
 		next = next[1:]
 	}
 }
--- day7/part2/part2.go
+++ day7/part2/part2.go
@@ -3,44 +3,91 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
-	"strings"
+	"regexp"
+	"sort"
 )
+
+type Node struct {
+	Prev, Next []string
+}
 
 func main() {
 	f, _ := os.Open("../input")
 	defer f.Close()
 	file := bufio.NewScanner(f)
-	file.Scan()
-	input := file.Text()
-	var loop []rune
-	for i := 0; i < 26; i += 1 {
-		loop = append(loop, rune('A'+i))
+	nodes := make(map[string]*Node)
+	for file.Scan() {
+		input := file.Text()
+		regex := regexp.MustCompile(`Step ([A-Z]) must be finished before step ([A-Z]) can begin.`)
+		res := regex.FindStringSubmatch(input)
+		_, ok := nodes[res[1]]
+		if !ok {
+			nodes[res[1]] = &Node{}
+		}
+		_, ok = nodes[res[2]]
+		if !ok {
+			nodes[res[2]] = &Node{}
+		}
+		nodes[res[1]].Next = append(nodes[res[1]].Next, res[2])
+		nodes[res[2]].Prev = append(nodes[res[2]].Prev, res[1])
 	}
-	min := 1000000000
-	for _, r := range loop {
-		next := strings.Replace(input, string(r), "", -1)
-		next = strings.Replace(next, string(r+32), "", -1)
-		result := react(next)
-		if result < min {
-			min = result
+	var next []string
+	completed := make(map[string]bool)
+	finish := make(map[string]int)
+	for key, value := range nodes {
+		if value.Prev == nil {
+			next = append(next, key)
+			finish[key] = time(key)
 		}
 	}
-	fmt.Println(min)
+
+	workers := make([]string, 5)
+	var t int
+	for t = 0; len(completed) < len(nodes); t += 1 {
+		for key, value := range finish {
+			if value == t {
+				completed[key] = true
+				for i, w := range workers {
+					if w == key {
+						workers[i] = ""
+					}
+				}
+				for _, nvalue := range nodes[key].Next {
+					_, ok := finish[nvalue]
+					if !ok {
+						all_done := true
+						for _, prev := range nodes[nvalue].Prev {
+							_, done := completed[prev]
+							if !done {
+								all_done = false
+								break
+							}
+						}
+						if all_done {
+							next = append(next, nvalue)
+						}
+					}
+				}
+			}
+		}
+
+		sort.StringSlice(next).Sort()
+		for i, v := range workers {
+			if v == "" && len(next) > 0 {
+				workers[i] = next[0]
+				finish[next[0]] = t + time(next[0])
+				next = next[1:]
+			}
+		}
+		fmt.Println(t)
+		fmt.Println(completed)
+	}
+	fmt.Println(t - 1)
+
 }
 
-func react(s string) int {
-	var result []rune
-	for _, r := range []rune(s) {
-		if len(result) > 0 && math.Abs(float64(r-result[len(result)-1])) == 32 {
-
-			result = result[:len(result)-1]
-
-			continue
-		}
-		result = append(result, r)
-	}
-
-	return len(result)
+func time(s string) int {
+	r := []rune(s)[0]
+	return int(r - 4)
 }
